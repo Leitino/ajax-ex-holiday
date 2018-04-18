61b78624b407ed4e970cedef0c28f299ecd23593
Leitino <alessandro.leita89@gmail.com>
2018-04-18 17:24:33 +0200

    primo punto

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 $(function() {
 
-  
 
+  $('.button').click(function() {
+    var country = $('#nazione').val()
+    var year = $('#anno').val()
+    var month = $('#mese').val()
+    $.ajax({
+      url:"https://holidayapi.com/v1/holidays",
+      method:"GET",
+      data:{
+        key:'54a6fac8-a41c-4f07-9392-cac57a3b19e9',
+        country: country,
+        year: year,
+        month: month,
+      },
+      success:function(data) {
+        var vacanze = data
+        console.log(vacanze)
+        var holidays = []
+        holidays.push(vacanze)
+        console.log(holidays)
+
+      },
+    })
+  })
 })
